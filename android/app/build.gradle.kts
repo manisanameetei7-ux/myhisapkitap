9867a032d57b6cf2e11f8a6d78c8292a757fa9ff
Manisana Meetei <manisanameetei7@gmail.com>
2026-06-06 15:36:24 +0530

    Add ABI split version codes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 import java.io.FileInputStream
 import java.util.Properties
+import com.android.build.gradle.internal.api.ApkVariantOutputImpl
 import org.gradle.api.GradleException
 
 plugins {
@@ -15,6 +16,7 @@ val hasReleaseKeystore = keystorePropertiesFile.exists()
 if (hasReleaseKeystore) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
 
 android {
     namespace = "hisapkitap.com"
@@ -55,6 +57,19 @@ android {
         release {
             if (hasReleaseKeystore) {
                 signingConfig = signingConfigs.getByName("release")
+            }
+        }
+    }
+
+    applicationVariants.configureEach {
+        val variant = this
+        variant.outputs.forEach { output ->
+            val abiVersionCode = abiCodes[
+                output.filters.find { it.filterType == "ABI" }?.identifier
+            ]
+            if (abiVersionCode != null) {
+                (output as ApkVariantOutputImpl).versionCodeOverride =
+                    variant.versionCode * 10 + abiVersionCode
             }
         }
     }
